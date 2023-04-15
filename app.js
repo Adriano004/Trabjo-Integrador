d3d08f42236ae43bc306066cb83b22fbbc10b146
AdrianoMedina <adrianomedina005@gmail.com>
2023-04-14 23:33:49 -0300

    firts commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// confetti({
+//     particleCount: 100,
+//     spread: 70,
+//     origin: { y: 0.6 }
+//   });
+
+//   function randomInRange(min, max) {
+//     return Math.random() * (max - min) + min;
+//   }
+  
+//   confetti({
+//     angle: randomInRange(55, 125),
+//     spread: randomInRange(50, 70),
+//     particleCount: randomInRange(50, 100),
+//     origin: { y: 0.6 }
+//   });
